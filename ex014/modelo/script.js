5ac9961f38fed37885e06872a3c185c9a8325508
bvasquesdev <brunovasques96@gmail.com>
2025-04-05 13:20:33 -0300

    contador finalizado

--- ex014/modelo/script.js
+++ ex014/modelo/script.js
@@ -5,15 +5,29 @@ function contar(){
     let res = document.getElementById('res');
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'Impossível contar!'
         window.alert('[ERRO] Faltam dados!');
     } else {
-        res.innerHTML = "Contando: "
+        res.innerHTML = "Contando: <br>"
         let i = Number(ini.value);
         let f = Number(fim.value);
         let p = Number(passo.value);
-
-        for (let c = i; c <= f; c += p){
-            res.innerHTML += `${c} `
+        if (p<=0){
+            window.alert('Passo inválido! Considerando PASSO 1');
+            p = 1;
         }
+        
+        if (i<f){
+            //contagem crescente
+            for (let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //contagem regressiva
+            for(let c = i; c>=f; c-=p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
