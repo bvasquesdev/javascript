66055d03b1b1d669d5d3ffb7b5159000035ee083
Bruno Vasques <brunovasques96@gmail.com>
2025-11-24 15:47:56 -0300

    js

---
+++ funcoes/script.js
@@ -0,0 +1,8 @@
+const repeatText = (text, repeat = 2) => {
+    for (let i = 0; i < repeat; i++){
+        console.log(text)
+        }
+};
+
+repeatText("Testando");
+repeatText("Agora repete 5 vezes", 5);
